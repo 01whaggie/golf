999326aa1e190e7ecc301b26bb3efc0ef6b42769
DokahlSoZoor <manu.hobert@gmail.com>
2016-03-15 14:55:38 +0100

    Map class can read json files now!

--- Map.java
+++ Map.java
@@ -1,30 +1,56 @@
+import javax.json.*;
+
+import java.io.FileReader;
 import java.util.ArrayList;
 
-/**
- * Created by Michael on 14.03.2016.
- */
 public class Map {
 
-    int sizeX, sizeY;
-    int[][] mapArray;
+	String path, name, desc;
 
-    public static ArrayList<Tile> defaultTiles = new ArrayList<>();
+	double width, height;
+	ArrayList<Double> walls;
 
-    static void loadTiles(String string) {
-        for (int i = 0; i < 10; i ++) {
-            //do shit
-        }
-    }
+	Map(String path) {
+		this.path = path;
+		try {
+			JsonReader jsonReader = Json.createReader(new FileReader(path));
+			JsonObject obj = jsonReader.readObject();
 
-    Map(int x, int y) {
-        sizeX = x;
-        sizeY = y;
+			this.name = obj.getJsonString("name").getString();
+			this.desc = obj.getJsonString("desc").getString();
+			this.width = obj.getJsonNumber("width").doubleValue();
+			this.height = obj.getJsonNumber("height").doubleValue();
 
-        mapArray = new int[x][y];
-    }
+			JsonArray arr = obj.getJsonArray("walls");
+			this.walls = new ArrayList<Double>();
+			for(int i = 0; i < arr.size(); i++){
+				JsonArray segment = arr.getJsonArray(i);
+				if(segment.size() != 4){
+					throw new Exception("Invalid wall format");
+				}
+				for(int j = 0; j < segment.size(); j++){
+					double d = segment.getJsonNumber(j).doubleValue();
+					walls.add(d);
+				}
+			}
+			jsonReader.close();
+		} catch(Exception e){
+			System.out.println("something went wrong while reading map: " + path);
+			e.printStackTrace();
+		}
+		// System.out.println(path);
+		// System.out.println(name);
+		// System.out.println(desc);
+		// System.out.println(width);
+		// System.out.println(height);
+		// System.out.println(walls);
+	}
 
+	public ArrayList<Double> getWalls(){
+		return walls;
+	}
 
-
-
-
+	public static void main(String[] args) {
+		Map m = new Map("test.json");
+	}
 }
